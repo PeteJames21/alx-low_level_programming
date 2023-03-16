d71787e99ee76fbe077513ead7e8a8c2bc5d2b51
pete <peetjames21@gmail.com>
2023-03-16 23:29:46 +0300

    Add solution for task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Print something using the `puts` function
+*
+* Return: 0
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
