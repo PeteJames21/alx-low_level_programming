72a80f078760a4145a99c40047d2c9b845273c85
PeteJames21 <peetjames21@gmail.com>
2023-03-20 17:39:10 +0300

    Add solution for task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Print all numbers from 0-9 using putchar
+*
+* Return: 0(success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
