1d78e514fa3d6dbc0840d935b08a61e3b0244883
PeteJames21 <peetjames21@gmail.com>
2023-03-20 16:21:33 +0300

    Remove mixed declaration after statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,11 +13,13 @@
 int main(void)
 {
 	int n;
+	int x;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	/*Calculate the last digit of n*/
-	int x = n % 10;
+	x = n % 10;
 
 	printf("Last digit of %d is %d and is ", n, x);
 
