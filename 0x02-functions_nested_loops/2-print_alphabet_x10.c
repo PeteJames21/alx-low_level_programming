fcb10e06948274a5f405fe18ba532eea8ae7b721
PeteJames21 <peetjames21@gmail.com>
2023-03-21 13:08:43 +0300

    Print all lowercase letters ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_alphabet_x10 - print all lowercase letters 10 times
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int rep;
+	char c;
+
+	for (rep = 1; rep <= 10; rep++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+
+		putchar('\n');
+	}
+}
