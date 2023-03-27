2e51a1903eeaf5eb981fea704dd2ae3f2e883376
PeteJames21 <peetjames21@gmail.com>
2023-03-27 23:57:19 +0300

    Print even-indexed chars of string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* puts2 - print the even-indexed characters of a string
+*
+* @str: pointer to the string to be printed
+*/
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != 0; i++)
+		if (i % 2 == 0)
+			_putchar(*(str + i));
+
+	_putchar('\n');
+}
