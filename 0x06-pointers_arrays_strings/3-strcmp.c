77de2ccc381de92031376eead04dbec4c9d63d38
PeteJames21 <peetjames21@gmail.com>
2023-03-30 13:05:25 +0300

    Implement strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _strcmp - compare two strings
+* @s1: first string to be compared
+* @s2: second string to be compared
+*
+* Return: 0 if s1 == s2, -15 if s1 < s2, 15 if s1 > s2
+*/
+int _strcmp(char *s1, char *s2)
+{
+	while (1)
+	{
+		if (*s1 < *s2)
+			return (-15);
+		else if (*s1 > *s2)
+			return (15);
+		else if (*s1 != '\0' && *s2 != '\0')
+			return (0);
+	}
+
+	return (0);
+}
