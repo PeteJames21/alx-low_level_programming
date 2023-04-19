1dc648b888f18c292f8abb6b16673889c4654aba
PeteJames21 <peetjames21@gmail.com>
2023-04-19 23:13:49 +0300

    Apply an arbitrary function to all array elements

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+
+/**
+* array_iterator - execute a function on each element of an array
+* @array: array to be operated on
+* @size: size of the array
+* @action: pointer to the function to apply on each element of the array
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array && action)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
