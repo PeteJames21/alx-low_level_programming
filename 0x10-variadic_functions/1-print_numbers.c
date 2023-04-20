8457a78344e9324d29e2571ca108b451655324cb
PeteJames21 <peetjames21@gmail.com>
2023-04-20 19:13:16 +0300

    Print numbers using a separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - print the integers provided, followed by a separator
+* @separator: string to be printed between the numbers
+* @n: number of integers passed
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	const char *sep;
+
+	if (!separator)
+		sep = "";
+	else
+		sep = separator;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+			printf("%d%s", va_arg(args, int), sep);
+		else
+			printf("%d\n", va_arg(args, int));
+	}
+	va_end(args);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -2,3 +2,9 @@
 
 ## 0-sum_them_all.c
 Write a function that returns the sum of all its parameters.
+
+## 1-print_numbers.c
+Write a function that prints numbers, followed by a new line.
+- where `separator` is the string to be printed between numbers
+- and `n` is the number of integers passed to the function
+- If separator is `NULL`, don’t print it
