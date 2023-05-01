500882488634dd8087f1617f794247c6c48675d6
PeteJames21 <peetjames21@gmail.com>
2023-05-01 22:56:08 +0300

    Allow head to be null

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,14 +15,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	unsigned int i = 0;
 
-	if (!*head)
+	if (!*head && idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (!new_node)
 		return (NULL);
 	new_node->n = n;
-	new_node->next = NULL;
 
 	if (idx == 0)
 	{
