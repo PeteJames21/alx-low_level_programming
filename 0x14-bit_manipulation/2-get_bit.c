96d938265b75a634f19a1a5c9fe5a2d9d2ae98b5
PeteJames21 <peetjames21@gmail.com>
2023-05-05 00:18:14 +0300

    Add a guard against out-of-range indexing

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -10,5 +11,10 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	/* Shift n by `index` and get the resulting least significant bit */
+
+	/* Error if index is larger than 1 less the number of bits */
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
 	return ((n >> index) & 1);
 }
