c9581101e88c4c10efcaf1d80dccd830922da229
PeteJames21 <peetjames21@gmail.com>
2023-08-17 12:42:07 +0300

    Return NULL in stead of exiting

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-		return(EXIT_FAILURE);
+		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
