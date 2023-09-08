bc0ed96d353f85015aec4502c4af52f9c5371470
PeteJames21 <peetjames21@gmail.com>
2023-09-08 20:22:14 +0300

    strdup keys and values before setting hashnode

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -28,8 +28,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node = malloc(sizeof(hash_node_t));
 		if (!node)
 			return (0);
-		node->key = (char *)key;
-		node->value = (char *)value;
+		node->key = strdup(key);
+		node->value = strdup(value);
 		node->next = NULL;
 		(ht->array)[index] = node;
 		return (1);
@@ -38,7 +38,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
-			node->value = (char *)value;
+			free(node->value);
+			node->value = strdup(value);
 			return (1);
 		}
 		node = node->next;
@@ -47,8 +48,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!node)
 		return (0);
 	tmp = (ht->array)[index];  /* The head of the linked list */
-	node->key = (char *)key;
-	node->value = (char *)value;
+	node->key = strdup(key);
+	node->value = strdup(value);
 	node->next = tmp;
 	(ht->array)[index] = node;
 
